456e83509fb5c0103cb2e8f9fbccfb75955e10b8
UjwalSingh7177 <ujwalsingh7177.com>
2025-09-29 10:48:51 +0530

    Template changes

--- src/app/profilecomponent/profilecomponent.ts
+++ src/app/profilecomponent/profilecomponent.ts
@@ -3,8 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  templateUrl: './profilecomponent.html',
+  styleUrls: ['./profilecomponent.css']
 })
 export class ProfileComponent implements OnInit {
   name = '';
